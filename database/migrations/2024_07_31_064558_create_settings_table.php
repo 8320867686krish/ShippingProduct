7291efa360f11b4e4fa22873c98c9d9a6598360d
Kaushik Panot <kaushik.panot@meetanshi.com>
2024-08-01 16:11:47 +0530

    Change product add api

--- database/migrations/2024_07_31_064558_create_settings_table.php
+++ database/migrations/2024_07_31_064558_create_settings_table.php
@@ -16,20 +16,23 @@ return new class extends Migration
                 $table->id();
                 $table->unsignedBigInteger('user_id');
                 $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-                $table->boolean('enabled');
-                $table->string('title', 100);
-                $table->string('shipping_rate', 20);
-                $table->string('shipping_rate_calculation', 20);
-                $table->string('method_name', 100);
-                $table->boolean('product_shipping_cost');
-                $table->integer('rate_per_item');
-                $table->integer('handling_fee');
-                $table->string('applicable_countries', 25);
-                $table->boolean('method_if_not_applicable');
-                $table->string('displayed_error_message');
-                $table->boolean('show_method_for_admin');
-                $table->integer('minimum_order_amount');
-                $table->timestamps();
+                $table->boolean('enabled')->default(0)->comment('1=Yes, 0=No');
+                $table->string('title');
+                $table->tinyInteger('shipping_rate')->comment('1=Per Item, 2=Per Order');
+                $table->tinyInteger('shipping_rate_calculation')->comment('1=Sum of Rate 2=Maximum Value 3=Minimum Value');
+                $table->string('method_name')->nullable();
+                $table->boolean('product_shipping_cost')->comment('1=Yes 0=No');
+                $table->decimal('rate_per_item', 8, 2)->nullable();
+                $table->decimal('handling_fee', 8, 2)->nullable();
+                $table->boolean('applicable_countries')->comment('0=All Allowed Countries 2=Specific Countries');
+                $table->longText('countries')->nullable();
+                $table->boolean('method_if_not_applicable')->comment('1=Yes 0=No')->nullable();
+                $table->text('displayed_error_message')->nullable();
+                $table->boolean('show_method_for_admin')->nullable()->comment('1=Yes 0=No');
+                $table->decimal('min_order_amount', 8, 2)->nullable();
+                $table->decimal('max_order_amount', 8, 2)->nullable();
+                $table->timestamp('created_at')->useCurrent();
+                $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
             });
         }
     }
