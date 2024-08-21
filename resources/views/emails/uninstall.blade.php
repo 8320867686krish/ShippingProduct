1433c551306331786fabc08dbe45cdee6af5ff57
Kaushik Panot <kaushik.panot@meetanshi.com>
2024-08-21 13:24:00 +0530

    Create Install and uninstall Support mail

--- resources/views/emails/uninstall.blade.php
+++ resources/views/emails/uninstall.blade.php
@@ -1,16 +1,3 @@
-{{-- <!DOCTYPE html>
-<html>
-<head>
-    <title>Account Uninstall Notification</title>
-</head>
-<body>
-    <h1>Hi {{ $name }},</h1>
-    <p>This is to notify you that your account associated with {{ $shopDomain }} has been uninstalled.</p>
-    <p>If you have any questions, feel free to contact us.</p>
-    <p>Thank you!</p>
-</body>
-</html> --}}
-
 <!doctype html>
 <html lang="en-US">
 
