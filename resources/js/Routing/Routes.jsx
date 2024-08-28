1188e767318c0fab1bf56b3452711d4bce0b34ec
khushi-patel12 <khushi.sonani@meetanshi.com>
2024-08-28 09:29:35 +0530

    changes

--- resources/js/Routing/Routes.jsx
+++ resources/js/Routing/Routes.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 import { Route } from "react-router";
 import { Routes } from 'react-router-dom';
 import Products from '../pages/Products';
-import Plans from '../pages/Plans';
-
+import Support from '../pages/Support';
 
 export default function Routing(props) {
     return (
         <Routes>
             <Route  index element={<Products {...props} />} />
-            {/* <Route  path="/Plans" element={<Plans {...props} />} /> */}
+            <Route  path="/helpCenter" element={<Support {...props} />} />
         </Routes>
     );
 }
