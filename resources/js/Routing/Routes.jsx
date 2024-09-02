f61da43102568daf4d346da2b59e347b4cc43fef
khushi-patel12 <khushi.sonani@meetanshi.com>
2024-09-02 18:42:59 +0530

    add plan page

--- resources/js/Routing/Routes.jsx
+++ resources/js/Routing/Routes.jsx
@@ -4,13 +4,14 @@ import { Routes } from 'react-router-dom';
 import Products from '../pages/Products';
 import Support from '../pages/Support';
 import Main from '../pages/Main';
-
+import Plans from '../pages/Plans';
 export default function Routing(props) {
     return (
         <Routes>
             <Route  index element={<Products {...props} />} />
             <Route exact path="/Main" element={<Main {...props} />} />
             <Route  path="/helpCenter" element={<Support {...props} />} />
+            <Route  path="/plan" element={<Plans {...props} />} />
         </Routes>
     );
 }
